bc0f8900254a09fa0589862e77f951b649fc51e5
Koudai Murakoshi <kokoronotomoyo@gmail.com>
2011-08-16 03:28:12 +0900

    homecheck

---
+++ AnzanCode/AnzanCode/class.h
@@ -0,0 +1,16 @@
+//
+//  class.h
+//  AnzanCode
+//
+//  Created by lissajous on 11/08/16.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface class : UIViewController {
+
+}
+
+@end
