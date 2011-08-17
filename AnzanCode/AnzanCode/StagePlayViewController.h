6076674009933c629952fbcae38e91303142d465
sutekicut <kokoronotomoyo@gmail.com>
2011-08-17 17:57:09 +0900

    stageplay  fixed

--- AnzanCode/AnzanCode/StagePlayViewController.h
+++ AnzanCode/AnzanCode/StagePlayViewController.h
@@ -34,19 +34,29 @@
 @interface StagePlayViewController : UIViewController {
     
     //乱数を作る
-    NSInteger random1;
-    NSInteger random2;
+    NSInteger random1;//一つ目の数字用
+    NSInteger random2;//２つ目の数字用
+    NSInteger decideOperation;//＋ー×÷を決定するための乱数
     
+    
+    //計算式の答え
     NSInteger result;
     
+    //入力された数式
     NSInteger input;
+    
+    
+    NSInteger Qnum;//問題数
+    
+    NSInteger rightAnswer;//正解数をカウントする
+    
 
     //問題が出力されるラベル
-    IBOutlet UILabel*   q1;
-    IBOutlet UILabel*   q2;
+    IBOutlet UILabel*   q1;//１つ目の数字
+    IBOutlet UILabel*   q2;//２つ目の数字
     
-    //入力した数字を表示するラベル
-    IBOutlet UILabel*   inputNum;
+    IBOutlet UILabel*   operation;//＋ー×÷表示するためのラベル
+    IBOutlet UILabel*   inputNum;//入力した数字を表示するラベル
     
 
     //ナンバータッチパネル
@@ -63,10 +73,10 @@
     
     
     
-    //enter
+    //enterボタン
     IBOutlet UIButton*  Enter;
     
-    //delete
+    //deleteボタン
     IBOutlet UIButton*  deleteNum;
     
 }
@@ -75,6 +85,7 @@
 //-----------------------------------
 @property (nonatomic, retain)UILabel*   q1;
 @property (nonatomic, retain)UILabel*   q2;
+@property (nonatomic ,retain)UILabel*   operation;
 @property (nonatomic, retain)UILabel*   inputNum;
 @property (nonatomic, retain)UIButton*  num1;
 @property (nonatomic, retain)UIButton*  num2;
@@ -105,6 +116,16 @@
 - (IBAction)input0: (id)sender;
 - (IBAction)inputEnter: (id)sender;
 - (IBAction)inputDelete: (id)sender;
+
+
+//　プライベートメソッド
+
+- (void) displayQuestion;//問題を表示するためのメソッド
+- (BOOL) correctOrIncorrect;//計算式と入力された値を比較して、正解、不正解を返す
+- (void) refleshQuestion;//次の問題にするために再表示するためのメソッド
+- (void) makeQuestion;//問題を作成するためのメソッド
+
+
 
 @end
 
--- AnzanCode/AnzanCode/StagePlayViewController.m
+++ AnzanCode/AnzanCode/StagePlayViewController.m
@@ -6,13 +6,25 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+
+
+
+
+
+/*
+ 課題：入力できる文字は２桁までにする
+ 
+ 
+ 
+ */
+
 #import "StagePlayViewController.h"
 
 
 @implementation StagePlayViewController
 
 
-@synthesize q1, q2, inputNum;
+@synthesize q1, q2, inputNum, operation;
 @synthesize num0,num1,num2,num3,num4,num5,num6,num7,num8,num9;
 @synthesize Enter, deleteNum;
 
@@ -67,23 +79,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    //２０までの乱数を取得
-    random1 = arc4random()%20;
+    [self makeQuestion];
     
-    //10までの乱数を取得
-    random2 = arc4random()%10;
-    
-    
-    NSString*   randomString1 = [NSString stringWithFormat:@"%d", random1];
-    NSString*   randomString2 = [NSString stringWithFormat:@"%d", random2];
-
-    NSLog(@"%@" , randomString1);
-    NSLog(@"%@" , randomString2);
-    
-    self.q1.text = randomString1;
-    self.q2.text = randomString2;
-    
-    
+    [self displayQuestion];
+        
     
 }
 
@@ -110,59 +109,149 @@
 
 - (IBAction)input1: (id)sender {
     
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"1"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"1"];
+        
+    }else {
+        
+    }
     
 }
 
 - (IBAction)input2: (id)sender {
     
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"2"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"2"];
+        
+    }else {
+        
+    }
+
 }
 
 - (IBAction)input3: (id)sender {
     
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"3"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"3"];
+        
+    }else {
+        
+    }
 }
 
 - (IBAction)input4: (id)sender {
-    
-    
+
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"4"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"4"];
+        
+    }else {
+        
+    }
 }
 
 - (IBAction)input5: (id)sender {
-    
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"5"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"5"];
+        
+    }else {
+        
+    }
 }
 
 - (IBAction)input6: (id)sender {
-    
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"6"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"6"];
+        
+    }else {
+        
+    }
 }
 
 - (IBAction)input7: (id)sender {
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"7"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"7"];
+        
+    }else {
+        
+    }
 }
 
 - (IBAction)input8: (id)sender {
-    
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"8"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"8"];
+        
+    }else {
+        
+    }
+
 }
 
 - (IBAction)input9: (id)sender {
-    
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"9"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"9"];
+        
+    }else {
+        
+    }
 }
 
 - (IBAction)input0: (id)sender {
-    
-    
+    if ([self.inputNum.text length] == 0) {
+        self.inputNum.text = [NSString stringWithFormat:@"0"];
+        
+    }else if([self.inputNum.text length] == 1) {
+        self.inputNum.text = [self.inputNum.text stringByAppendingFormat:@"0"];
+        
+    }else {
+        
+    }
 }
 
 - (IBAction)inputEnter: (id)sender {
     
+    //正解か、不正解かをチェックする
+    if ([self correctOrIncorrect]) {//正解なら
+        //丸付け！
+        
+        //正解問題数のカウントをあげる
+    }else {
+        
+        
+        
+    }
     
 }
 
 - (IBAction)inputDelete: (id)sender {
+    
+    self.inputNum.text = [self.inputNum.text substringToIndex:[self.inputNum.text length] - 1];
     
     
 }
@@ -172,28 +261,129 @@
 #pragma mark -util-
 //-----------------------------------
 
-//四則演算を決定する
-
-- (void) sisoku {
+//問題を表示するためのメソッド
+- (void) displayQuestion {
     
-    NSInteger   sisoku = arc4random()%3;
+    NSString*   questionOne = [NSString stringWithFormat:@"%d", random1];
+    self.q1.text = questionOne;
     
-    switch (sisoku) {
-        //足し算
+    NSString*   questionTwo = [NSString stringWithFormat:@"%d", random2];    
+    self.q2.text = questionTwo;
+    
+    switch (decideOperation) {
         case 0:
+            self.operation.text = [NSString stringWithFormat:@"＋"];
             
             break;
-        //引き算
+            
         case 1:
-            
+            self.operation.text = [NSString stringWithFormat:@"ー"];
             
             break;
-        //かけ算    
+            
+            
         case 2:
+            self.operation.text = [NSString stringWithFormat:@"×"];
             
             break;
-        //割り算    
+            
         case 3:
+            
+            self.operation.text = [NSString stringWithFormat:@"÷"];
+            
+            break;
+            
+        default:
+            break;
+    }
+}
+
+
+//計算式と入力された値を比較して、正解、不正解を返す
+- (BOOL) correctOrIncorrect {
+    
+    
+    
+}
+
+
+//次の問題にするために再表示するためのメソッド
+- (void) refleshQuestion {
+    
+    //まず問題数のカウントを行って、
+    
+    //まだ問題の残りがあるなば、問題を作成して、。表示
+    if (Qnum < 10) {
+        [self makeQuestion];
+    
+        
+        
+        
+    }else  {//すでに問題数が上限ならば終了して、結果を表示する
+    
+        
+        
+        
+    }
+    
+    
+}
+
+
+//問題を作成するためのメソッド
+- (void) makeQuestion {
+    
+    
+    decideOperation = arc4random()%3;//乱数からOperationを決定して、数字を保持＋表示
+    
+    
+    //計算して、計算結果を保持
+    switch (decideOperation) {
+        case 0://足し算
+            random1 = arc4random()%10;//乱数から数式１を作成して、保持＋表示
+            random2 = arc4random()%10;//乱数から数式２を作成して、保持＋表示
+            
+            result = random1 + random2;
+            break;
+            
+        case 1://引き算
+            //引き算は答えがマイナスにならないように考慮
+            
+            random1 = arc4random()%10;//乱数から数式１を作成して、保持＋表示
+            random2 = arc4random()%10;//乱数から数式２を作成して、保持＋表示
+            
+            if (random2 > random1) {//もし２番目の方が大きかったら
+                NSInteger temp = random1;
+                random1 = random2;
+                random1 = temp;
+                result = random2 - random1;
+            }else {
+                result = random1 - random2;
+            }
+            
+            break;
+            
+        case 2://かけ算
+            random1 = arc4random()%10;//乱数から数式１を作成して、保持＋表示
+            random2 = arc4random()%10;//乱数から数式２を作成して、保持＋表示
+            
+            result = random1 * random2;
+            
+            break;
+            
+            
+        case 3://割り算
+            random1 = arc4random()%10;//乱数から数式１を作成して、保持＋表示
+            random2 = arc4random()%10;//乱数から数式２を作成して、保持＋表示
+            
+            //割り算は、余りがでないように考慮
+            while ((random1 % random2) != 0) {
+                //作り直し
+                random1 = arc4random()%10;//乱数から数式１を作成して、保持＋表示
+                random2 = arc4random()%10;//乱数から数式２を作成して、保持＋表示
+            }
+            
+            result = random1 / random2;
             
             
             break;
@@ -202,14 +392,6 @@
         default:
             break;
     }
-    
-}
-
-
-//問題をリフレッシュする
-- (void)reflesh {
-    
-    
     
 }
 
